8f4a2deae20af09dadaa6fd1fd200a32a61987c8
texast99 <travis.bonneau@gmail.com>
2019-08-02 12:28:31 -0500

    Add mouse functionality

--- Source.cpp
+++ Source.cpp
@@ -11,6 +11,59 @@
 using namespace std;
 using namespace cv;
 
+Mat image, other, blurImage;
+int blurSize, key, col, delta;
+bool isInfoVisible, isMouseDown;
+
+void updateImage();
+static void onMouse(int event, int x, int y, int flags, void* param);
+void copySubImage(Mat&, Mat&, int, int);
+
+
+bool inRange(int r, int c, int minR, int maxR, int minC, int maxC) {
+	return (r >= minR && r <= maxR) && (c >= minC && c <= maxC);
+}
+
+int getInRange(int minNum, int num, int maxNum) {
+	return max(minNum, max(num, minNum));
+}
+
+void updateImage() {
+	copySubImage(image, other, col, image.cols);
+	copySubImage(blurImage, other, 0, col);
+	if (isInfoVisible) {
+		putText(other, "Slide Velocity: " + to_string(delta), Point(10, 20), FONT_HERSHEY_DUPLEX, 0.5, Scalar(0, 0, 0), 1);
+		putText(other, "Blur Kernel Size: " + to_string(blurSize), Point(10, 35), FONT_HERSHEY_DUPLEX, 0.5, Scalar(0, 0, 0), 1);
+	}
+	line(other, Point(col, 0), Point(col, other.rows), Scalar(255, 255, 255), 1);
+	imshow("Gaussian Blur Visualizer", other);
+}
+
+static void onMouse(int event, int x, int y, int flags, void* param) {
+	if (isMouseDown && !inRange(y, x, 0, image.rows - 1, 0, image.cols - 1)) {
+		return;
+	}
+		
+	switch (event)
+	{
+		case EVENT_LBUTTONDOWN:
+			isMouseDown = true;
+			col = getInRange(0, x, image.cols - 1);
+			updateImage();
+			break;
+		case EVENT_LBUTTONUP:
+			isMouseDown = false;
+			break;
+		case EVENT_MOUSEMOVE:
+			if (isMouseDown) {
+				col = getInRange(0, x, image.cols - 1);
+				updateImage();
+			}
+		default:
+			break;
+	}
+}
+
 void copySubImage(Mat& src, Mat& dst, int col1, int col2) {
 	if (src.size() != dst.size())
 		return;
@@ -34,27 +87,20 @@ int main(int argc, const char* argv[]) {
 		cout << "Too few arguments, requires 2" << endl;
 		return -1;
 	}
+	namedWindow("Gaussian Blur Visualizer", WINDOW_AUTOSIZE);
+	setMouseCallback("Gaussian Blur Visualizer", onMouse, 0);
 
-	Mat image, other, blur;
-	int blurSize = 25;
-	bool isInfoVisible = false;
-
+	blurSize = 25;
 	image = imread(argv[1], IMREAD_COLOR);
-	GaussianBlur(image, blur, Size(blurSize, blurSize), 0);
+	GaussianBlur(image, blurImage, Size(blurSize, blurSize), 0);
 	other = Mat(image.size(), image.type());
 
-	int key;
-	int col = image.cols / 2;
-	int delta = 1;
+	col = image.cols / 2;
+	delta = 1;
+	isInfoVisible = false;
 
 	do {
-		copySubImage(image, other, col, image.cols);
-		copySubImage(blur, other, 0, col);
-		if (isInfoVisible) {
-			putText(other, "Slide Velocity: " + to_string(delta), Point(10, 20), FONT_HERSHEY_DUPLEX, 0.5, Scalar(0, 0, 0), 1);
-			putText(other, "Blur Kernel Size: " + to_string(blurSize), Point(10, 35), FONT_HERSHEY_DUPLEX, 0.5, Scalar(0, 0, 0), 1);
-		}
-		imshow("Gaussian Blur Visualizer", other);
+		updateImage();
 		key = waitKeyEx(0);
 
 		switch (key) {
@@ -73,12 +119,12 @@ int main(int argc, const char* argv[]) {
 			case 'w':
 			case 'W':
 				blurSize = min(blurSize + 2, 101);
-				GaussianBlur(image, blur, Size(blurSize, blurSize), 0);
+				GaussianBlur(image, blurImage, Size(blurSize, blurSize), 0);
 				break;
 			case 's':
 			case 'S':
 				blurSize = max(blurSize - 2, 1);
-				GaussianBlur(image, blur, Size(blurSize, blurSize), 0);
+				GaussianBlur(image, blurImage, Size(blurSize, blurSize), 0);
 				break;
 			case 't':
 			case 'T':
@@ -88,6 +134,7 @@ int main(int argc, const char* argv[]) {
 				break;
 		}
 	} while (key != 27 && key != -1);
+
 	destroyAllWindows();
 	return 0;
 }
